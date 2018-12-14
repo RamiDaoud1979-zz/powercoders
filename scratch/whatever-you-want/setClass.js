f58d7388356357324a4f81d17872e2f3b52b0b71
Rami Daoud <rami.daoud@powercoders.org>
2018-12-14 14:31:28 +0100

    refactoring js code

---
+++ scratch/whatever-you-want/setClass.js
@@ -0,0 +1,15 @@
+// Javascript code goes here
+function setClassTip(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'tip';
+}
+
+function setClassWarning(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'warning';
+}
+
+function clearClass(event) {
+  let el = document.getElementById('mypara');
+  el.className = '';
+}
