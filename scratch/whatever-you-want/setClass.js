dfb1dba476fe620d4ac300b51166f20e3ca58738
Rami Daoud <rami.daoud@powercoders.org>
2018-12-20 13:50:29 +0100

    chaining

--- scratch/whatever-you-want/setClass.js
+++ scratch/whatever-you-want/setClass.js
@@ -1,5 +1,17 @@
-
-function setClass(event, name) {
+function setClass(className) {
   let el = document.getElementById('mypara');
-  el.className = name;
+  el.className = className;
 }
+
+document.addEventListener('DOMContentLoaded', function (event){
+  document.getElementById('make-warning').addEventListener('click', function (event){
+    setClass('warning');
+  });
+
+  document.getElementById('make-tip').addEventListener('click', function (event) {
+    setClass('tip');
+  });
+    document.getElementById('make-normal').addEventListener('click', function (event) {
+      setClass('');
+  });
+});
