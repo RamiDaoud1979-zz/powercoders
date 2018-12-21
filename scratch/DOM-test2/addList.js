d0d4a0263e9d7862af84c1fdfc51409e77ceb18d
Rami Daoud <rami.daoud@powercoders.org>
2018-12-21 15:21:47 +0100

    adding some comments

--- scratch/DOM-test2/addList.js
+++ scratch/DOM-test2/addList.js
@@ -1,18 +1,18 @@
 
 function addList() {
+    // ul and body
     let elUl = document.createElement('ul');
     let elBody = document.getElementById('body');
-
-
+    //List one
     let elLi1 = document.createElement('li');
     let liContentTextLi1 = document.createTextNode('Item one');
     elLi1.appendChild(liContentTextLi1);
     elUl.appendChild(elLi1);
-
-let elLi2 = document.createElement('li');
+// list two
+    let elLi2 = document.createElement('li');
     let liContentText1 = document.createTextNode('This is a ');
     elLi2.appendChild(liContentText1);
-
+//em under lists 2
     let liContentEm = document.createElement('em');
     elLi2.appendChild(liContentEm);
     let liContentEmText = document.createTextNode('new');
@@ -20,7 +20,7 @@ let elLi2 = document.createElement('li');
     let liContentText2 = document.createTextNode(' text');
     elLi2.appendChild(liContentText2);
     elUl.appendChild(elLi2);
-
+//list three
     let elLi3 = document.createElement('li');
     let liContentTextLi3 = document.createTextNode('Item Three');
     elLi3.appendChild(liContentTextLi3);
