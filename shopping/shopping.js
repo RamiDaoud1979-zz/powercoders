43149c565a6a2da7b9c45fa11df5487a83897d51
Rami Daoud <rami.daoud@powercoders.org>
2019-01-08 09:35:30 +0100

    Write a function that creates a list item upon clicking on a button

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,6 +1,27 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-   document.querySelector('button').addEventListener('click', function(event) {
-       let el = document.getElementById('item');
-        console.log(el.value);
-    });
+function createNewListItem(itemName) {
+    //Create li node
+    let li = document.createElement('li');
+
+    // create span node
+    let span = document.createElement('span');
+    // let spanText = document.createTextNode(itemName);
+    // li.appendChild(spanText);
+    // Alternatively...
+    span.innerText = itemName;
+    li.appendChild(span);
+
+    // create button node
+    let button = document.createElement('button');
+    // create button text node
+    let buttonText = document.createTextNode('delete');
+    // append text node to button
+    button.appendChild(buttonText);
+    li.appendChild(button);
+    return li;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+   document.querySelector('button').addEventListener('click', function() {
+       document.querySelector('ul').appendChild(createNewListItem(document.getElementById('item').value));
+      });
 });
