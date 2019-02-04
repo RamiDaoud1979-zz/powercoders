8e248f0fdf3867aee258bd1b87025b199dd5f2f0
Rami Daoud <rami.daoud@powercoders.org>
2019-02-04 12:05:24 +0100

    Split js to ui and shoppinglist

--- shopping-class/ui.js
+++ shopping-class/ui.js
@@ -1,46 +1,4 @@
 /**
- * Represent an item in the shopping list.
- */
-class ShoppingListItem {
-  /**
-   * @param name {string} name of the item
-   * @param quantity {string} quantity of the item
-   */
-  constructor(name, quantity) {
-    this.name = name;
-    this.quantity = quantity;
-  }
-
-  /**
-   * Create and returns an 'li' element for inclusion in the shopping list.
-   * @returns {HTMLElement} li element
-   */
-  toListItem() {
-    const li = document.createElement('li');
-    const listText = document.createElement('span');
-    listText.innerText = this.name;
-    li.appendChild(listText);
-
-    if (this.quantity !== '') {
-      li.appendChild(document.createTextNode(' '));
-      const quantityText = document.createElement('span');
-      quantityText.textContent = `(${this.quantity})`;
-      li.appendChild(quantityText);
-    }
-
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = '<img src="delete.png" alt="delete"/>';
-    deleteButton.addEventListener('click', function (event) {
-      li.remove();
-      document.querySelector('button#clear').disabled = document.querySelectorAll('li').length === 0;
-    });
-    li.appendChild(deleteButton);
-    return li;
-  }
-}
-
-
-/**
  *  Set up even listeners and configure initial element state when the
  *  DOM is ready
  */
